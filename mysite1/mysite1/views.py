f877c3a0cabaf2b4a2548684e816c858520c4c9a
kkkikki <kaseykim7123@gmail.com>
2024-07-18 11:52:08 +0900

    django test added

---
+++ mysite1/mysite1/views.py
@@ -0,0 +1,16 @@
+#from typing import Any
+from django.views.generic.base import TemplateView
+from django.apps import apps
+
+class HomeView(TemplateView):
+    template_name = 'home.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        #context['app_list'] = ['polls', 'books']
+        dictVerbose = {}
+        for app in apps.get_app_configs():
+            if 'packages' not in app.path:
+                dictVerbose[app.label] = app.label
+        context['verbose_dict'] = dictVerbose
+        return context
