09387cdb5babeb87effc23020d5a4dda982fea8e
root <root@ip-172-31-8-253.ap-northeast-2.compute.internal>
2024-07-23 07:55:45 +0000

    delete vod

--- vod/stream/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.video_list, name='video_list'),
-    path('video/<int:video_id>/', views.video_player, name='video_player'),
-]
