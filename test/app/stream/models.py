ecb8b29ac18e3971d0df377e49f32ed499e42f6e
root <root@psb>
2024-07-29 11:50:03 +0900

    delete test

--- test/app/stream/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-class Video(models.Model):
-    title = models.CharField(max_length=200)
-    thumbnail_url = models.URLField()
-    video_url = models.URLField()
-
-    def __str__(self):
-        return self.title
